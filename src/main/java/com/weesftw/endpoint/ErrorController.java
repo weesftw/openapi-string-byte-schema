fd9d2ce3c44264dde5040b76d9fc2fb18ee14dee
Wesley <wees.phone@gmail.com>
2022-03-17 16:16:23 -0300

    Update error endpoint

--- src/main/java/com/weesftw/endpoint/ErrorController.java
+++ src/main/java/com/weesftw/endpoint/ErrorController.java
@@ -2,17 +2,15 @@ package com.weesftw.endpoint;
 
 import com.weesftw.Error;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/error")
 public class ErrorController
 {
-    @GetMapping
-    public ResponseEntity<Error> foo()
+    @PostMapping
+    public ResponseEntity<Error> foo(Error error)
     {
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok(error);
     }
 }
