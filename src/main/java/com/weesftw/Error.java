a52a680920d17b8e5f2c479bee54cbb09fb49ff0
Wesley <wees.phone@gmail.com>
2022-03-22 08:58:27 -0300

    Fix schema attributes

--- src/main/java/com/weesftw/Error.java
+++ src/main/java/com/weesftw/Error.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotNull;
 @Schema(description = "Error schema")
 public class Error
 {
-    @Schema(name = "message", required = true)
+    @Schema(name = "message", required = true, type = "string", format = "byte")
     @JsonProperty("message")
     @lombok.Builder.Default
     @NotNull
@@ -26,29 +26,11 @@ public class Error
     }
 
     @NotNull
-    @Schema(required = true)
     public byte[] getMessage() {
         return message;
     }
 
     public void setMessage(byte[] message) {
         this.message = message;
-    }
-
-    @Override
-    public boolean equals(java.lang.Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Error erro = (Error) o;
-        return Objects.equals(this.message, erro.message);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(message);
     }
 }
